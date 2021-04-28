9b9467058482746b30a5b4ea6f3d522038fa33a8
lchipham <79922787+lchipham@users.noreply.github.com>
2021-04-28 15:02:54 +0700

    Add files via upload

---
+++ Deck_CS.py
@@ -0,0 +1,44 @@
+from PlayingCard import*
+from random import*
+
+class Deck:
+    """A class to represent a deck of cards"""
+    def __init__(self):                          #initializes deck class
+        suit = ["c", "d", "h", "s"]              #creates list of potential suits
+        self.deck = []                           #creates empty list to store card values ---> suits and ranks
+        for suits in suit:                       #loops through suits
+            for ranks in range(1,14):            #loops through ranks
+                card = PlayingCard(ranks, suits) #generates cards with each possible suit and rank using PlayingCard class
+                self.deck.append(card)           #appends created card to self.deck (4 suits * 13 ranks = 52 total cards) to create deck
+
+
+    def shuffle(self):                         #method to shuffle cards
+        shuffledCards = []                     #list to store shuffled cards
+        for oldCard in range(len(self.deck)):  #loops through/shuffles each of the 52 cards in "old" positions of list self.deck
+            newCard = randrange(0,52)          #generates a random rank number value to replace "old" card value with a "new" card value in list self.deck 
+            self.deck[oldCard], self.deck[newCard] = self.deck[newCard], self.deck[oldCard] #shuffles cards; takes card in position "oldCard" of cardList                                                                                           
+        return self.deck  #returns shuffled deck of cards                                    #and replaces it with randomly generated "newCard" 52x  
+
+    def dealCard(self):                          #method to deal first card from the top of the deck and remove it from deck
+        return self.deck.pop(0)                  #pops off first item in deck and returns value of card
+        
+
+    def cardsLeft(self):                         #method to return the number of cards left in deck
+        return len(self.deck)                    #returns the length of current deck when called
+
+
+###TEST
+##def main():
+##    n = int(input("How many cards would you like to deal out? "))
+##    myDeck = Deck()
+##    myDeck.shuffle()
+##    for i in range(n):
+##        print(myDeck.dealCard())
+##main()                
+            
+            
+            
+            
+            
+                
+                
---
+++ buttonClass.py
@@ -0,0 +1,68 @@
+from graphics import *
+class Button:
+    """A button is a labeled rectangle in a window.
+    It is activated or deactivated with the activate() and deactivate() methods.
+    The isClicked() method returns True if and only if the button is activated and
+    pt is inside its boundaries."""
+
+    def __init__(self,win,label,centerPt,height,width):
+        """Create a rectangular button in a graphical window with the words label
+        in it located at Point centerPt and with height being how tall the button
+        and width being how wide the button is, e.g
+        myButton = Button(myWin, "Quit", Point(300,300), 50,100)"""
+        x = centerPt.getX()
+        y = centerPt.getY()
+        self.xmin = x - width/2
+        self.xmax = x + width/2
+        self.ymin = y - height/2
+        self.ymax = y + height/2
+        pt1 = Point(self.xmin, self.ymin)
+        pt2 = Point(self.xmax, self.ymax)
+        self.rect = Rectangle(pt1,pt2)
+        self.words = Text(centerPt, label)
+        self.rect = Rectangle(pt1,pt2)
+        self.rect.draw(win)
+        self.rect.setFill("lavender")
+        self.words.draw(win)
+        self.activate()
+
+    def activate(self):
+        """Set this button to be enabled/active"""
+        self.words.setFill("black")
+        self.rect.setWidth(2)
+        self.active = True
+    def deactivate(self):
+        ##color the text dark grey
+        self.words.setFill("light gray")
+        ##set poutline of button to be thinner
+        self.rect.setWidth(1)
+        ##set the boolean active flag t be false
+        self.active = False
+        
+    def isClicked(self,point):
+        """Returns True if button active and Point is inside, else returns False"""
+        if self.xmin <= point.getX() <= self.xmax \
+            and self.ymin <= point.getY() <= self.ymax:
+                return True
+        else:
+            return False
+def main():
+    #creates a new Graphwin object
+    myWin = GraphWin("test",600,600)
+    #create a new Button object
+    #ie. instantiate the Button class to test it out
+    myButton = Button(myWin, "Quit", Point(300,300),50,100)
+    myButton.deactivate()
+    pt = myWin.getMouse()
+    #yButton.deactivate()
+    while not myButton.isClicked(pt):
+        #if the window is clicked, then activate button
+        if not myButton.isClicked(pt):
+            myButton.activate()
+        pt = myWin.getMouse()
+    myWin.close()
+if __name__ == '__main__':
+    main()
+
+
+    
